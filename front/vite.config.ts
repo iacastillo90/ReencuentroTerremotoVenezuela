666410154054499dc7ffbe77b395a845cf2748eb
IvanCastillo <iacastillo.ili90@gmail.com>
2026-06-27 22:47:05 -0400

    feat: offline mode, cache en redis y panel para mascotas

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\/api\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 // 24 hours
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: 'Reencuentro Terremotos Venezuela',
+        short_name: 'ReencuentroVE',
+        description: 'Plataforma para centralizar búsquedas de personas y mascotas en desastres.',
+        theme_color: '#0f172a',
+        icons: [
+          {
+            src: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+            sizes: '25x41',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
