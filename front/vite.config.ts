e768be4214c6122c81e8b95e87aa4e8dc3190192
IvanCastillo <iacastillo.ili90@gmail.com>
2026-07-12 23:46:49 -0400

    Fix (Dev): Configurar proxy para WebSockets (/socket.io) en entorno local de Vite.

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -48,6 +48,11 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:4000',
         changeOrigin: true
+      },
+      '/socket.io': {
+        target: 'http://localhost:4000',
+        ws: true,
+        changeOrigin: true
       }
     }
   }
